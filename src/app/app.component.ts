428d2d17fffefc018d714b2f7243f56ec3be3098
ricardosribeiro <ricardo.sribeiro@outlook.com>
2021-06-07 23:52:11 -0300

    feat: rendering a list of articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,16 @@ import { Article } from 'src/shared/Article';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  articles: Article[] = [];
+  articles: Article[] = [
+    new Article('Angular', 'http://angular.io', 3),
+    new Article('Fullstack', 'http://fullstack.io', 2),
+    new Article('Angular Homepage', 'http://angular.io', 1)
+  ];
 
-  addArticle(title:HTMLInputElement, link:HTMLInputElement): boolean{
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     this.articles.push(new Article(title.value, link.value));
     // console.log(`${this.articles[0].title}, ${this.articles[0].link}`)
     return true;
   }
-  
+
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../shared/Article';
 
 @Component({
@@ -8,17 +8,9 @@ import { Article } from '../../shared/Article';
 })
 export class ArticleComponent {
 
-  article: Article;
+  @Input() article: Article;
 
-  constructor() {
-    this.article = new Article(
-      'Angular',
-      'http://angular.io',
-      10
-    );
-
-
-  }
+  constructor() { }
 
   voteUp(): boolean {
     this.article.votes += 1;
